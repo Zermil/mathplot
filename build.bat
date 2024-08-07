ebedd1110cd051ff296d3c8adc1d5eebbb4555ed
Aiden <marcin.madry@interia.com>
2024-08-07 17:39:03 +0200

    Change build

--- build.bat
+++ build.bat
@@ -2,8 +2,9 @@
 
 REM Change this to your visual studio's 'vcvars64.bat' script path
 set MSVC_PATH="C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build"
-set CFLAGS=/EHsc /W4 /WX /FC /wd4533 /wd4505 /wd4996 /nologo /fsanitize=address /Zi 
-set CFLAGS_RELEASE=/EHsc /W4 /WX /FC /wd4533 /wd4505 /wd4996 /nologo /O2 /DNDEBUG /DUSE_OPTICK=0
+set CFLAGS=/EHsc /W4 /WX /FC /wd4533 /wd4505 /wd4996 /nologo 
+set DEBUG_FLAGS=/fsanitize=address /Zi
+set RELEASE_FLAGS=/O2 /DNDEBUG /DUSE_OPTICK=0
 set INCLUDES=/I"code\dependencies"
 set LIBS=user32.lib shell32.lib comdlg32.lib "external\freetype.lib"
 set D3D11_LIBS=dxgi.lib dxguid.lib d3d11.lib d3dcompiler.lib
@@ -15,7 +16,7 @@ pushd %~dp0
 if not exist .\build mkdir build
 
 if "%1" == "release" (
-    cl %CFLAGS_RELEASE% %INCLUDES% "code\main.cpp" /Fo:build\ /Fe:build\mathplot.exe /link %LIBS% %D3D11_LIBS% /ignore:4099
+    cl %CFLAGS% %RELEASE_FLAGS% %INCLUDES% "code\main.cpp" /Fo:build\ /Fe:build\mathplot.exe /link %LIBS% %D3D11_LIBS% /ignore:4099
     
     if exist ".\build\OptickCore.dll" del ".\build\OptickCore.dll"
     
@@ -24,10 +25,10 @@ if "%1" == "release" (
     del ".\build\*.exp"
     del ".\build\*.lib"
 ) else (
-    cl %CFLAGS% %INCLUDES% "code\main.cpp" /Fo:build\ /Fe:build\mathplot.exe /link %LIBS% %D3D11_LIBS% ".\external\OptickCore.lib" /ignore:4099
+    cl %CFLAGS% %DEBUG_FLAGS% %INCLUDES% "code\main.cpp" /Fo:build\ /Fe:build\mathplot.exe /link %LIBS% %D3D11_LIBS% ".\external\OptickCore.lib" /ignore:4099
     
     move ".\*.pdb" ".\build\"
-    copy ".\external\OptickCore.dll" ".\build\"
+    if not exist ".\build\OptickCore.dll" copy ".\external\OptickCore.dll" ".\build\"
 )
 
 popd
